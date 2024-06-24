29d6c5e6a9a1709c65f8c1d5b2f89906a416e260
Evelien-Vissers <vissersevelien@gmail.com>
2024-06-24 19:34:26 +0200

    Testen

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -116,5 +116,6 @@ public class Person {
         return grandChildren;
     }
 
+
 }
 
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -43,4 +43,47 @@ assertEquals("Jan", result);
         assertEquals(2, result.size());
 
     }
+    @Test
+    public void testAddParents() {
+    //Arrange
+        Person jan = new Person ("Jan", "Janssen", 40, "Male");
+        Person moeder = new Person ("Anna", "Janssen", 78, "Female");
+        Person vader = new Person ("Richard", "Janssen", 87, "Male");
+    //Act
+        jan.addParents(moeder, vader);
+    //Assert
+    assertEquals(moeder, jan.getMother());
+    assertEquals(vader, jan.getFather());
+    }
+
+    @Test
+    public void testAddPet() {
+    //Arrange
+        Person jan = new Person ("Jan", "Janssen", 40, "Male");
+        Pet hond = new Pet("Buddy", 11, "Dog");
+    //Act
+        jan.addPet(hond);
+    //Assert
+        assertEquals(hond, jan.getPets().getFirst());
+    }
+    @Test
+    public void testAddSibling() {
+    //Arrange
+        Person jan = new Person ("Jan", "Janssen", 40, "Male");
+        List<Person> siblings = new ArrayList<>();
+        Person marie = new Person ("Marie", "Janssen", 40, "Female");
+        siblings.add(marie);
+        jan.setSiblings(siblings);
+        //Act
+        List<Person> result = jan.getSiblings();
+    //Assert
+        assertEquals(1, result.size());
+    }
+@Test
+    public void testGetGrandChildren() {
+    //Arrange
+    Person grootvader = new Person ("Piet", "Janssen", 89, "Male");
+    Person vader = new Person("Henk", "Janssen", 56, "Male");
+
+}
 }
